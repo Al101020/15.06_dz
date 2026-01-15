64783fe1099798984adc975bd5f4f7b181af57ac
Al101020 <alex.vl.m@yandex.ru>
2026-01-15 17:09:59 +1000

    commit-3

--- src/components/task1/WorldClock.jsx
+++ src/components/task1/WorldClock.jsx
@@ -2,7 +2,7 @@ function WorldClock() {
   return (
     <>
       <div className='task1'>
-        <h5>WorldClock</h5>
+        <h5 className='h5'>WorldClock</h5>
       </div>
     </>
   )
--- src/components/task2/Crud.jsx
+++ src/components/task2/Crud.jsx
@@ -1,8 +1,32 @@
+import './crud.css';
+
 function Crud() {
+  fetch('http://localhost:7070/notes')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP ошибка! Код: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log('Данные из API:', data);
+    })
+    .catch(error => {
+      console.error('Ошибка при выполнении запроса:', error);
+    });
+
+
+
   return (
     <>
       <div className='task2'>
-        <h5>Crud</h5>
+        <div className='title'>
+          <div className='notes'>
+            <h1 className='notesH1'>Notes</h1>
+          </div>
+          <button className='update'>Обнавить</button>
+        </div>
+        <div className='crud'></div>
       </div>
     </>
   )
