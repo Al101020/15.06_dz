da60694ae4df001cb61acf54c43622671c71d3e3
Al101020 <alex.vl.m@yandex.ru>
2026-01-17 13:00:04 +1000

    commit-6

--- src/components/task1/WorldClock.jsx
+++ src/components/task1/WorldClock.jsx
@@ -1,8 +1,27 @@
+import './worldClock.css';
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log('Клик кнопка Добавить Часы');
+};
+
 function WorldClock() {
   return (
     <>
       <div className='task1'>
-        <h5 className='h5'>WorldClock</h5>
+        <h1 className='titleTask'>WorldClock</h1>
+        <form onSubmit={handleSubmit}>
+          <div id='wcTitle'>
+            <label>Название
+            <input type="text" name="title" placeholder="может London" /></label>
+          </div>
+          <div id='wcTimeZone'>
+            <label>Временная зона
+            <input type="text" name="timeZone" placeholder="например 10" /></label>
+          </div>
+          
+          <input type="submit" name="submitInfo" value="Добавить"></input>
+        </form>
       </div>
     </>
   )
--- src/components/task2/Crud.jsx
+++ src/components/task2/Crud.jsx
@@ -1,34 +1,47 @@
 import './crud.css';
 
+const fetchGet = () => fetch('http://localhost:7070/notes')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP ошибка! Код: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log('Клик кнопка Обновить');
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Ошибка при выполнении запроса:', error);
+  });
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log('Клик кнопка Добавить');
+};
+
 function Crud() {
-  fetch('http://localhost:7070/notes')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP ошибка! Код: ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Данные из API:', data);
-    })
-    .catch(error => {
-      console.error('Ошибка при выполнении запроса:', error);
-    });
-
-
+  // fetchGet();
 
   return (
     <>
       <div className='task2'>
         <div id='crudTitle'>
           <div className='notes'>
-            <h1 className='notesH1'>Notes</h1>
+            <h1 className='titleTask'>Notes</h1>
           </div>
-          <button className='update'>Обнавить</button>
+          <button id='updateBtn' onClick={fetchGet}>Обнавить</button>
         </div>
         <div id='allNotes'></div>
-        <div id='crudBottom'></div>
-        {/* <div className='crud'></div> */}
+        <div id='crudBottom'>
+          <div>New Note</div>
+          <div>
+            <form onSubmit={handleSubmit}>
+              <textarea name="comment" rows="5" cols="45"></textarea><br></br>
+              <input type="submit" name="submitInfo" value="Отправить"></input>
+            </form>
+          </div>
+        </div>
       </div>
     </>
   )
