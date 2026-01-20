1131455cedf0ec91415c76422d433c2e1ab1bfe6
Al101020 <alex.vl.m@yandex.ru>
2026-01-20 17:25:17 +1000

    commit-10r

--- src/components/task1/WorldClock copy 3.jsx
+++ src/components/task1/WorldClock copy 3.jsx
@@ -2,50 +2,52 @@ import './worldClock.css';
 import { useEffect, useState } from 'react';
 import moment from 'moment'; // import moment_timezone from 'moment-timezone';
 
-const clock0 = () => { //(title, timeZone)
-  const clockMoment = moment().utcOffset(0).format("HH:mm:ss");
-  return clockMoment;
-}
-
-
 function WorldClock() {
   const [title, setTitle] = useState('');
   const [timeZone, setTimeZone] = useState('');
-  const [allWClock, setAllWClock] = useState([]);
 
-  
+  const clock = (timeZone) => { //(title, timeZone)
+    const clockMoment = moment().utcOffset(timeZone).format("HH:mm:ss");
+    const clockMoment0 = moment().utcOffset(0).format("HH:mm:ss");
+    
+    return (
+      console.log(clockMoment0),
+      console.log(timeZone),
+      clockMoment
+    );
+  }
   
   function handleSubmit(e) {
     e.preventDefault();    // console.log('Клик кнопка Добавить Часы');
 
-    console.log(title);
+    // const valueInputTitle = document.querySelector('#title');
+    // console.log(title);
     const valueTitle = title;
-    console.log(timeZone);
+    // console.log(timeZone);
 
-    setAllWClock({
-      title: title,
-      timeZone: timeZone
-    });
 
-    console.log(allWClock);
-
-    setTitle('');
-    setTimeZone('');
-
-    let timeLondon = clock0();
+    const timeWc = () => {
+      console.log(timeZone);
+      const clockWc = clock(timeZone);
+      console.log(clockWc);
+      return clockWc;
+    };
     
     const allWc = document.querySelector('#allWc');
       const divClock = document.createElement('div');
       divClock.classList.add('divClock');
         const clockTitle = document.createElement('div');
         clockTitle.classList.add('clockTitle');
-        clockTitle.textContent = valueTitle + ' - ' + 'время(ЧЧ:мм:сс):';
+        clockTitle.textContent = valueTitle + ' - ';
         const clock = document.createElement('div');
         clock.classList.add('clock');
-        clock.textContent = ' ' + timeLondon;
+        clock.textContent = {timeWc};
       divClock.append(clockTitle);
       divClock.append(clock);
     allWc.append(divClock);
+    
+    setTitle('');
+    setTimeZone('');
   };
 
   return (
@@ -54,16 +56,14 @@ function WorldClock() {
         <h1 className='titleTask'>WorldClock</h1>
         <form onSubmit={handleSubmit} id='formWC' name='formWC'>
           <div id='wcTitle'>
-            <label>Название
-              <input type='text' name='title' value={title} 
-              onChange={(e) => setTitle(e.target.value)} placeholder='может г. Уфа' required />
-            </label>
+            <label>Название<input type='text' name='title' value={title} 
+            onChange={(e) => setTitle(e.target.value)} placeholder='может г. Уфа' /></label>
           </div>
           <div id='wcTimeZone'>
             <label>Временная зона
               <input type='number' min='-12' max="12" name='timeZone' 
               value={timeZone} onChange={(e) => setTimeZone(e.target.value)} 
-              placeholder='от -12 до 12' style={{width: '100px'}} required />
+              placeholder='от -12 до 12' style={{width: '100px'}} />
             </label>
           </div>
           <input type='submit' name='formWC' value='Добавить'></input>
